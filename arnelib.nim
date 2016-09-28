86a62de685e10022453fc91220d4dd95b44cbaf7
Arne Döring <arne.doering@gmx.net>
2016-09-28 15:33:47 +0200

    joined files, added nimble file

--- arnelib.nim
+++ arnelib.nim
@@ -223,3 +223,54 @@ when isMainModule:
     age    = 28
     
   assert s"Hello, my name is $myName, I am $age years old. In five years I am ${age + 5}" == "Hello, my name is Arne, I am 28 years old. In five years I am 33"
+
+  ###############
+  # ########### #
+  # # cast of # #
+  # ########### #
+  ###############
+
+
+import macros
+
+macro castof*(sym:typed, t : typedesc): expr =
+  # introduce a new identifier with the same name as the input identifier
+  # this identifier will have the type t, but hide sym
+  let ident = newIdentNode($sym.symbol)
+  result = quote do:
+    (let `ident` = cast[`t`](`sym`); `sym` of `t`)
+
+macro namedEcho*(x: typed): expr =
+  let lit = newLit(x.repr & ": ")
+  quote do:
+    echo `lit`, `x`
+    
+if isMainModule:    
+  type
+    MyContainer[T] = object of RootObj
+      data : seq[T]
+
+    MyObject = object of RootObj
+      banone : int
+
+  var container1 = MyContainer[  int32](data: @[1'i32, 2,3,4,5])
+  var container2 = MyContainer[float32](data: @[5'f32, 4,3,2,1]) 
+  var banone : MyObject
+
+  let all = @[
+    (ptr RootObj)(container1.addr),
+    container2.addr,
+    banone.addr,
+    nil
+  ]
+
+  for x in all:
+    if x.castof(ptr MyObject):
+      echo "it is Myobject ", x.banone
+    elif x.castof(ptr MyContainer[int32]):
+      echo "it is int32 ", x.data.len
+    elif x.castof(ptr MyContainer[float32]):
+      echo "it is float32 ", x.data.len
+    else:
+      echo "don't know the type"
+      
---
+++ arnelib.nimble
@@ -0,0 +1,7 @@
+# Package
+
+version       = "1.0.0"
+author        = "Arne Döring"
+description   = "My personal utilities library that I like to share accross projects"
+license       = "BSD"
+requires "nim >= 0.12.0"
