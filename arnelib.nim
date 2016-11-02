60a404852b9c03b178b018e23a2c80e860fa087e
Arne Döring <arne.doering@gmx.net>
2016-11-02 23:59:43 +0100

    random

--- arnelib.nim
+++ arnelib.nim
@@ -1,7 +1,48 @@
-import macros, sequtils, algorithm
+import macros, sequtils, algorithm, mersenne, ziggurat_normal_dist, math
 
+var mt = newMersenneTwister(seed = 123)
+
+proc randNormal*(): float64 = mt.nextGaussian()
+
+proc rand_u64*(): uint64 =
+  (uint64(mt.getNum) shl 32) or uint64(mt.getNum)
+
+proc rand_u32*(): uint32 = mt.getNum
+
+proc rand_i32*(): int32 = rand_u32().int32
+
+proc rand_i64*(): int64 = rand_u64().int64
+
+proc rand_f64*(): float64 =
+  rand_u64().float64 / pow(2'f64, 64'f64)
+
+proc rand_f32*(): float32 =
+  rand_u32().float32 / pow(2'f32, 32'f32)
+
+proc rand*(maxval: uint64): uint64 =
+  let limit = uint64(-1) - (uint64(-1) mod maxval)
+  var bits = rand_u64()
+  while bits > limit:
+    bits = rand_u64()
+  result = bits mod maxval
+
+proc rand*(maxval: uint32): uint32 =
+  let limit = uint32(high(uint32)) - uint32(high(uint32)) mod maxval
+  var bits = rand_u32()
+  while bits > limit:
+    bits = rand_u32()
+  result = bits mod maxval
+
+proc rand*(maxval: int32): int32 =
+  assert(maxval > 0)
+  rand(maxval.uint32).int32
+
+proc rand*(maxval: int64): int64 =
+  assert(maxval > 0)
+  rand(maxval.uint64).int64
+  
 ## sequence operations ##
-
+  
 proc newSeq*[T](length, capacity: Natural): seq[T] =
   assert(length <= capacity)
   result.newSeq(capacity)
